26d0026703c9d98f2caa0bc34f55d5c418af6273
mobin afshari <mma137813781378@gmail.com>
2025-04-11 12:28:10 +0330

    feat: preview section completed

--- scripts/index.js
+++ scripts/index.js
@@ -22,3 +22,40 @@ const jsEditor = CodeMirror.fromTextArea(document.getElementById("js"), {
   lineWrapping: true,
 });
 
+function updatePreview() {
+  const htmlContent = htmlEditor.getValue();
+  const cssContent = cssEditor.getValue();
+  const jsContent = jsEditor.getValue();
+
+  const iframe = document.getElementById("preview");
+  const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+
+  const content = `
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <style>
+          ${cssContent} 
+        </style>
+      </head>
+      <body>
+        ${htmlContent} 
+        <script>
+          ${jsContent} 
+        </script>
+      </body>
+    </html>
+  `;
+
+  iframeDoc.open();
+  iframeDoc.write(content);
+  iframeDoc.close();
+}
+
+htmlEditor.on("change", updatePreview);
+cssEditor.on("change", updatePreview);
+jsEditor.on("change", updatePreview);
+
+updatePreview();
